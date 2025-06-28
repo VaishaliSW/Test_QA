ab93dccb15ef163ba26241dff7f0ac49c4efa250
VaishaliSW <vaishali.waghmare@capgemini.com>
2025-06-28 13:50:41 +0530

    12

--- demo/code_block_8.java
+++ demo/code_block_8.java
@@ -1,24 +1,40 @@
-// File: DashboardPage.java
 package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class DashboardPage {
+public class WithdrawalPage {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    private By withdrawalLink = By.id("withdrawalLink");
+    private By withdrawalAmountField = By.id("withdrawalAmount");
+    private By withdrawalButton = By.id("withdrawalButton");
+    private By withdrawalSuccessMessage = By.id("withdrawalSuccessMessage");
 
-    public DashboardPage(WebDriver driver) {
+    public WithdrawalPage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, 10);
     }
 
-    public void navigateToWithdrawalPage() {
-        driver.findElement(withdrawalLink).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("withdrawalForm")));
+    public void navigateToWithdrawalPage(String baseUrl) {
+        driver.get(baseUrl + "/withdrawal");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(withdrawalAmountField));
+    }
+
+    public void enterWithdrawalAmount(String amount) {
+        driver.findElement(withdrawalAmountField).sendKeys(amount);
+    }
+
+    public void clickWithdrawalButton() {
+        driver.findElement(withdrawalButton).click();
+    }
+
+    public boolean isWithdrawalSuccessful() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(withdrawalSuccessMessage));
+        WebElement successMessage = driver.findElement(withdrawalSuccessMessage);
+        return successMessage.isDisplayed();
     }
 }
