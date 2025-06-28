2fa7afbded7bb61d92c39cfb92afbc49219d7391
VaishaliSW <vaishali.waghmare@capgemini.com>
2025-06-28 13:50:45 +0530

    12

--- demo/code_block_5.java
+++ demo/code_block_5.java
@@ -1,24 +1,24 @@
-// File: ConfigReader.java
 package utils;
 
-import org.json.JSONObject;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.FileReader;
 import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class ConfigReader {
     private static JSONObject configData;
 
     static {
         try {
-            String content = new String(Files.readAllBytes(Paths.get("config/config.json")));
-            configData = new JSONObject(content);
-        } catch (IOException e) {
+            JSONParser parser = new JSONParser();
+            configData = (JSONObject) parser.parse(new FileReader("config/config.json"));
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
     }
 
     public static String getConfigData(String key) {
-        return configData.getString(key);
+        return (String) configData.get(key);
     }
 }
