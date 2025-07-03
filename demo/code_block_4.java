0ec0fe8df0eea03a9a47c1bf407a0f99cd73d130
VaishaliSW <vaishali.waghmare@capgemini.com>
2025-07-03 18:18:36 +0530

    committed

--- demo/code_block_4.java
+++ demo/code_block_4.java
@@ -6,28 +6,58 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import java.util.concurrent.TimeUnit;
+import org.json.JSONObject;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class DriverManager {
-    private static WebDriver driver;
+    private WebDriver driver;
+    private String browser;
+    private String driverPath;
 
-    public static WebDriver getDriver(String browser) {
+    public DriverManager() {
+        loadConfig();
+        initializeDriver();
+    }
+
+    private void loadConfig() {
+        try {
+            String configData = new String(Files.readAllBytes(Paths.get("config/config.json")));
+            JSONObject config = new JSONObject(configData);
+            browser = config.getString("browser");
+            driverPath = config.getJSONObject("driverPath").getString(browser);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeDriver() {
         switch (browser.toLowerCase()) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+                System.setProperty("webdriver.chrome.driver", driverPath);
                 driver = new ChromeDriver();
                 break;
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", "path/to/geckodriver");
+                System.setProperty("webdriver.gecko.driver", driverPath);
                 driver = new FirefoxDriver();
                 break;
             case "edge":
-                System.setProperty("webdriver.edge.driver", "path/to/edgedriver");
+                System.setProperty("webdriver.edge.driver", driverPath);
                 driver = new EdgeDriver();
                 break;
             default:
-                throw new IllegalArgumentException("Browser not supported");
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public WebDriver getDriver() {
         return driver;
+    }
+
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
