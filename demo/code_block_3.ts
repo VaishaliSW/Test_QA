12ee42ae2e56123681c0e4a128b710ab5a98e87b
VaishaliSW <vaishali.waghmare@capgemini.com>
2025-09-29 21:24:09 +0530

    1

---
+++ demo/code_block_3.ts
@@ -0,0 +1,19 @@
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  private page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async navigate() {
+    await this.page.goto('/');
+  }
+
+  async login(username: string, password: string) {
+    await this.page.fill('#user-name', username);
+    await this.page.fill('#password', password);
+    await this.page.click('#login-button');
+  }
+}
